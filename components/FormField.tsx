15dca0dab4d28779f28e63500e8135cb7190ea4b
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-04-22 22:14:09 +0530

    eye icon add

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FormControl,
-  FormDescription,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa6";
 
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
@@ -16,31 +16,48 @@ interface FormFieldProps<T extends FieldValues> {
   placeholder?: string;
   type?: "text" | "email" | "password" | "file";
 }
+
 const FormField = <T extends FieldValues>({
   control,
   name,
   label,
   placeholder,
   type = "text",
-}: FormFieldProps<T>) => (
-  <Controller
-    name={name}
-    control={control}
-    render={({ field }) => (
-      <FormItem>
-        <FormLabel className="label">{label}</FormLabel>
-        <FormControl>
-          <Input
-            className="input"
-            placeholder={placeholder}
-            type={type}
-            {...field}
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
-);
+}: FormFieldProps<T>) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <div className="relative">
+            <FormControl>
+              <Input
+                className="input pr-10"
+                placeholder={placeholder}
+                type={isPassword ? (showPassword ? "text" : "password") : type}
+                {...field}
+              />
+            </FormControl>
+
+            {isPassword && (
+              <span
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+              </span>
+            )}
+          </div>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
 
 export default FormField;
