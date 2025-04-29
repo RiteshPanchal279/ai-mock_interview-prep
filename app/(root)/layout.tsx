9a81f3eb1419190a565cd355814ab5895aa0414b
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-04-29 10:42:39 +0530

    logo anf style updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,8 +12,8 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
-          <h3 className="text-primary-100">PrepWise</h3>
+          <Image src="/logo.svg" alt="Logo" width={58} height={52} />
+          <h3 className="text-primary-100">MyPrep</h3>
         </Link>
       </nav>
       {children}
